e246e08c18aac3f7441182a92a1dc02294576928
sereinof <1099209547@qq.com>
2024-04-06 21:03:29 +0800

    力扣78题子集回溯加剪枝的解法

--- nacosTest/src/main/java/person/man/zhouyuqing/test.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/test.java
@@ -6,11 +6,43 @@ import com.alibaba.nacos.api.config.ConfigService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 @SpringBootApplication
 public class test {
     public static void main(String[] args) {
-     SpringApplication.run(test.class,args);
+        List res =  subsets(new int[]{1, 2, 3});
+        System.out.println(res);
+        SpringApplication.run(test.class, args);
     }
+
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        int[] ints = {};
+        ArrayList res = new ArrayList<List<Integer>>();
+        ArrayList subset = new ArrayList();
+        travse(false, subset, 0, res, nums);
+        travse(true, subset, 0, res, nums);
+        return res;
+    }
+
+    public static void travse(Boolean isChooes, ArrayList subset, int level, ArrayList res, int[] nums) {
+        if (level > nums.length - 1) {
+            return;
+        }
+        if (isChooes == true) {
+            //  ArrayList temp = new ArrayList();
+            subset.add(nums[level]);
+            res.add(subset.toArray());
+        }
+        travse(false, subset, level + 1, res, nums);
+        travse(true, subset, level + 1, res, nums);
+        if (subset.size() > 0&&isChooes) {
+            subset.remove(subset.size() - 1);
+        }
+    }
+
 }
