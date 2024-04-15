4ccaa54175b8a7992d83c2feb1ded878a58fd46f
sereinof <1099209547@qq.com>
2024-04-15 11:44:29 +0800

    会议室

--- nacosTest/src/main/java/person/man/zhouyuqing/meetingRoom.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/meetingRoom.java
@@ -1,7 +1,26 @@
 package person.man.zhouyuqing;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class meetingRoom {
     public int minMeetingRooms(int[][] intervals) {
-
+        if(intervals.length==0){
+            return 0;
+        }
+        Arrays.sort(
+                intervals,
+                (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(intervals.length,(a,b) -> a - b);
+        queue.add(intervals[0][1]);
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]>=queue.peek()){
+                queue.poll();
+            }else{
+                queue.add(intervals[i][1]);
+            }
+        }
+        return  queue.size();
     }
 }
