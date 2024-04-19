79f5e3e8673f4eb7def093547586a9ff67f00423
sereinof <1099209547@qq.com>
2024-04-19 22:52:10 +0800

    代码格式化

--- nacosTest/src/main/java/person/man/zhouyuqing/twoSum.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/twoSum.java
@@ -2,6 +2,7 @@ package person.man.zhouyuqing;
 
 
 import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 //有序数组的和 (Two Sum II - Input array is sorted)
@@ -9,10 +10,20 @@ import java.util.HashMap;
 //题目描述： 给定一个已按照升序排列的整数数组 numbers 和一个目标值 target，请你在数组中找出和为目标值的那两个整数，并返回他们的数组下标（下标从1开始计数）。
 public class twoSum {//1,3,4,5,5,7  7
     public static void main(String[]args){
+        ThreadLocal<String> a = new ThreadLocal<>();
+        a.set("nihao");
+        a.get();
   int[] res =   new twoSum().twoSum(new int[]{1,3,4,5,5,7},7);
         System.out.println("finish"+res[0]+res[1]);
+        Boolean e =  null;
+        if(e ==true){
+            System.out.println("你好呀！！");
+        }
     }
     public  int[] twoSum(int [] nums ,int targer){
+        synchronized (new String()){
+            System.out.println("");
+        }
         int res;
        HashMap<Integer,Integer> map = new HashMap();
         for(int i = 0;i<nums.length;i++){
