fe921fa48ce3968d1bb80e7bb15a79435363dd4d
sereinof <1099209547@qq.com>
2024-04-21 23:29:04 +0800

    并查集根据身高重新排序

--- nacosTest/src/main/java/person/man/zhouyuqing/Sort.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/Sort.java
@@ -1,9 +1,28 @@
 package person.man.zhouyuqing;
 
+import java.util.Arrays;
+
 public class Sort {
     public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people,(a,b)->{
+            if(a[0]==b[0]){
+                 return b[1]-b[1];
+            }else {
+                return a[0] - b[0];
+            }
+        });
 
-
-        return  new int[][]{};
+        for(int i =0;i<people.length;i++){
+            if(people[i][1]==i){
+                continue;
+            }else if(people[i][1]<i){
+                int[] tmp = people[i];
+                for (int j =i; j>people[i][1];j--){
+                    people[j] = people[j-1];
+                }
+                people[i] = tmp;
+            }
+        }
+        return  people;
     }
 }
