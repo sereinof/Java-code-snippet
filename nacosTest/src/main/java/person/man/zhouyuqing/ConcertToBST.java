43743d1cc9da5b2f1a648642f27d4e4415d5b0c6
sereinof <1099209547@qq.com>
2024-04-30 21:21:35 +0800

    将有序数组转化为二叉搜索树

--- nacosTest/src/main/java/person/man/zhouyuqing/ConcertToBST.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/ConcertToBST.java
@@ -4,15 +4,18 @@ import java.util.Arrays;
 
 public class ConcertToBST {
     public static void main(String[] args) {
-
+        TreeNode result = new ConcertToBST().sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+        System.out.println(result);
     }
+
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums.length == 0) {
             return null;
         }
         TreeNode res = new TreeNode();
-        int mid = nums[nums.length / 2];
-        res.val = nums[mid];
+        int mid = nums.length / 2;
+        int mid_val = nums[mid];
+        res.val = mid_val;
         res.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, mid));
         res.right = sortedArrayToBST(Arrays.copyOfRange(nums, mid + 1, nums.length));
         return res;
