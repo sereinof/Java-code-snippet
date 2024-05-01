0c467b52ee9a1838b1c280fe2bf1ccbfcd65e59e
sereinof <1099209547@qq.com>
2024-05-01 23:24:16 +0800

    数组的最后一个元素加1

--- nacosTest/src/main/java/person/man/zhouyuqing/AddBinary.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/AddBinary.java
@@ -1,5 +1,9 @@
 package person.man.zhouyuqing;
 
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class AddBinary {
     public static void main(String[] args) {
         String res = new AddBinary().addBinary("101111", "10");
@@ -73,7 +77,7 @@ public class AddBinary {
                     if (hasN) {
                         res = "1" + res;
                         hasN = false;
-                           len1--;
+                        len1--;
                     } else {
                         res = "0" + res;
                         len1--;
@@ -94,5 +98,56 @@ public class AddBinary {
             res = "1" + res;
         }
         return res;
+    }
+
+    public int hammingWeight(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if (n == 0) {
+                return res;
+            }
+            int now = n & 1;
+            if (now == 1) {
+                res++;
+            }
+
+            n >>>= 1;
+        }
+        return res;
+    }
+
+    public int[] plusOne(int[] digits) {
+        if (digits[digits.length - 1] < 9) {
+            digits[digits.length - 1]++;
+            return digits;
+        } else {
+            digits[digits.length - 1] = digits[digits.length - 1] + 1 - 10;
+        }
+        boolean has = true;
+        for (int i = digits.length - 2; i >= 0; i--) {
+            if (has == true) {
+                if (digits[i] + 1 > 9) {
+                    digits[i] = digits[i] + 1 - 10;
+                    has = true;
+                } else {
+                    digits[i] = digits[i] + 1;
+                    has = false;
+                }
+            } else {
+                break;
+            }
+        }
+        if (has) {
+
+            int[] newArray = new int[digits.length + 1];
+
+// 在新数组的第一个位置插入 1
+            newArray[0] = 1;
+
+// 使用 System.arraycopy 将原始数组复制到新数组的后面
+            System.arraycopy(digits, 0, newArray, 1, digits.length);
+            return newArray;
+        }
+        return digits;
     }
 }
