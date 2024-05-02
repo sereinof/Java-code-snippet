5c1147f8fa270eac4ef7d322c5a85510fd808afd
sereinof <1099209547@qq.com>
2024-05-02 11:21:34 +0800

    删除数组中重复的元素 ，这题用快慢指针是真的骚啊

--- nacosTest/src/main/java/person/man/zhouyuqing/Sqrt.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/Sqrt.java
@@ -2,8 +2,9 @@ package person.man.zhouyuqing;
 
 public class Sqrt {
     public static void main(String[] args) {
-        int res = new Sqrt().mySqrt(1);
-        System.out.println(res);
+       // int res = new Sqrt().mySqrt(1);
+        //System.out.println(res);
+        new Sqrt().removeDuplicates(new int[]{1,1,1,2,2,3});
     }
 
     public int mySqrt(int x) {
@@ -22,5 +23,36 @@ public class Sqrt {
             }
         }
         return res;
+    }
+
+    int len = 0;
+
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        int j = 0;
+        len = nums.length;
+        while (j < len) {
+            while ( j<len&& nums[i] == nums[j]) {
+                j++;
+            }
+          int num =   remove(nums, i, j);//处理区间I ，j之间的重复数字，
+            i = j;
+            i-=num;
+            j-=num;
+        }
+        return len;
+
+    }
+
+    public int remove(int[] nums, int index1, int index2) {
+        if (index2 - index1 <= 2) {
+            return 0;//超出2才处理
+        }
+        int need = index2 - index1 - 2;
+        for (int i = index2; i < len; i++) {
+            nums[i - need] = nums[i];
+        }
+        len= len-need;
+        return  need;
     }
 }
