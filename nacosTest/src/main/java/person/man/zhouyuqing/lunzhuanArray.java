ea36b2fafd9f6417a5ad22939146fff586337cae
sereinof <1099209547@qq.com>
2024-05-02 11:43:53 +0800

    轮转数组 --但是这个超出时间了哇

--- nacosTest/src/main/java/person/man/zhouyuqing/lunzhuanArray.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/lunzhuanArray.java
@@ -1,7 +1,7 @@
 package person.man.zhouyuqing;
 
-public class lunzhuanArray {
-    public void rotate(int[] nums, int k) {
+public class lunzhuanArray11
+    public void rotate_bak(int[] nums, int k) {
         int len = nums.length;
         int point = k % len;
         int temp = nums[len - 1];
@@ -16,4 +16,19 @@ public class lunzhuanArray {
             nums[0] = temp;
         }
     }
+        public void rotate(int[] nums, int k) {
+            int len = nums.length;
+            int point = k % len;
+            int temp = nums[len - 1];
+            //这个题目看起来没这么简单！！
+            for (int i = 0; i < point; i++) {
+                temp = nums[len - 1];
+                int j = len - 1;
+                while (j > 0) {
+                    nums[j] = nums[j - 1];
+                    j--;
+                }
+                nums[0] = temp;
+            }
+        }
 }
