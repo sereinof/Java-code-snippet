6daa1d64b162d59e46f7ddbc172c500b7c2d8471
sereinof <1099209547@qq.com>
2024-05-04 21:34:31 +0800

    根据中序和后序构建二叉树

---
+++ nacosTest/src/main/java/person/man/zhouyuqing/Tree.java
@@ -0,0 +1,32 @@
+package person.man.zhouyuqing;
+
+public class Tree {
+    int[] inorder;
+    int[] postorder;
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.inorder = inorder;
+        this.postorder = postorder;
+        TreeNode root = buldTree1(0, inorder.length - 1, 0, postorder.length - 1);
+        return root;
+    }
+
+    public TreeNode buldTree1(int index1, int index2, int index3, int index4) {
+        if (index1 > index2) {
+            return null;
+        }
+        if (index1 == index2) {
+            return new TreeNode(inorder[index1]);
+        }
+        TreeNode root = new TreeNode(postorder[index4]);
+        int Rl = 0;
+        for (int i = index1; i <= index2; i++) {
+            if (inorder[i] == postorder[index4]) {
+                Rl = i - index1;
+            }
+        }
+        root.left = buldTree1(index1, index1 + Rl - 1, index3, index3 + Rl - 1);
+        root.right = buldTree1(index1 + Rl+1, index2, index3 + Rl, index4 - 1);
+        return root;
+    }
+}
