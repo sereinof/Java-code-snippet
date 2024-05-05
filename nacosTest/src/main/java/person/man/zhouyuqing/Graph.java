cb6e62725199a59c6af176464df75163234dcc3e
sereinof <1099209547@qq.com>
2024-05-05 23:42:14 +0800

    图的广度优先搜索

--- nacosTest/src/main/java/person/man/zhouyuqing/Graph.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/Graph.java
@@ -23,7 +23,7 @@ public class Graph {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<Integer> inN = new ArrayList<>(numCourses);
         ArrayList<List<Integer>> pointer = new ArrayList<>(numCourses);
-        for(int i =0;i<numCourses;i++){
+        for (int i = 0; i < numCourses; i++) {
             inN.add(0);
             pointer.add(new ArrayList<>());
         }
@@ -60,4 +60,64 @@ public class Graph {
             return new int[]{};
         }
     }
+
+
+    public int snakesAndLadders(int[][] board) {
+        int n1 = board.length * board[0].length;
+        int n = board.length ;
+        int[] endCor = id2rc(n1 , n);
+        int endP = board[endCor[0]][endCor[1]];
+        if (endP < n1 && endP != -1) {
+            return -1;//终点有蛇
+        }
+        boolean[] viisted = new boolean[n1 + 1];//记录是否访问过
+        Queue<Integer> queue = new ArrayDeque();
+        queue.offer(1);
+        int times = 0;
+        viisted[1] = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            times++;
+            for (int i = 0; i < size; i++) {
+                int posiotn = queue.poll();
+                for (int j = 1; j <= 6; j++) {
+                    int[] rc = id2rc(posiotn + j, n);
+                    int nxt = posiotn + j;
+                    if(nxt>n1){
+                        break;
+                    }
+                    if (posiotn + j >= n1) {
+                        return times;
+                    }
+                    int t = board[rc[0]][rc[1]];
+                    if (t == -1) {
+                    } else {
+                        if (t == n1) {
+                            return times;
+                        }
+                        nxt = t;
+                    }
+                    //还要判断当前的节点是否为-1 不然还多了一个可能
+                    if (viisted[nxt] == false) {
+                        viisted[posiotn] = true;
+                        queue.offer(nxt);
+                    }
+                }
+
+            }
+
+
+        }
+        return -1;
+
+    }
+
+    public int[] id2rc(int id, int n) {
+        int r = (id - 1) / n, c = (id - 1) % n;
+        if (r % 2 == 1) {
+            c = n - 1 - c;
+        }
+        return new int[]{n - 1 - r, c};
+    }
+
 }
