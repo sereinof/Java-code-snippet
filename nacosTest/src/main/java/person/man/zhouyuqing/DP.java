3734f261ef446dccfd560cc2a61d73074cfcc5f0
sereinof <1099209547@qq.com>
2024-05-07 20:58:46 +0800

    动态规划 交错字符串

--- nacosTest/src/main/java/person/man/zhouyuqing/DP.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/DP.java
@@ -29,4 +29,46 @@ public class DP {
         }
         return dp[x - 1][y - 1];
     }
+
+    public static void main(String[] args) {
+        new DP().isInterleave("aabc","abad","aabadabc");
+    }
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int size1 = s1.length();
+        int size2 = s2.length();
+        int size3 = s3.length();
+        if (size1 + size2 != size3) {
+            return false;
+        }
+        boolean[][] dp = new boolean[size1 + 1][size2 + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= size1; i++) {
+            dp[i][0] = s1.substring(0, i).equals(s3.substring(0, i));
+        }
+        for (int j = 1; j <= size2; j++) {
+            dp[0][j] = s2.substring(0, j).equals(s3.substring(0, j));
+        }
+        for (int i = 1; i <= size1; i++) {
+            for (int j = 1; j <= size2; j++) {
+                if (s1.charAt(i - 1) != s3.charAt(i + j - 1) && s2.charAt(j - 1) != s3.charAt(i + j - 1)) {
+                    dp[i][j] = false;
+                } else if (s1.charAt(i - 1) == s3.charAt(i + j - 1) && s2.charAt(j - 1) == s3.charAt(i + j - 1)){
+                    dp[i][j] = dp[i - 1][j]||dp[i][j - 1];
+                } else if (s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    System.out.println("iftere?");
+                }
+            }
+        }
+        return dp[size1][size2];
+    }
+
+    public int maxProfit(int[] prices) {
+
+    }
+
 }
