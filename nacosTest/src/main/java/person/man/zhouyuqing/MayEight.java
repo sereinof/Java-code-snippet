06e27c260a73c1589c621b710ed9f4e824932d4b
sereinof <1099209547@qq.com>
2024-05-08 16:40:34 +0800

    二分查找

--- nacosTest/src/main/java/person/man/zhouyuqing/MayEight.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/MayEight.java
@@ -155,4 +155,20 @@ public class MayEight {
         return dp[len-1];
 
     }
+    public int search(int[] nums, int target) {
+    int l =0;
+    int r = nums.length-1;
+    while ((r!=l)){
+        int mid = l+(r-l)>>1;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[mid]<target){
+            l=mid+1;
+        }else{
+            r=mid-1;
+        }
+    }
+    return l;
+    }
 }
