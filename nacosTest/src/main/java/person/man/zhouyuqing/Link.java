3d363fead0282f196eb1fe7d8d24ff67c54f6991
sereinof <1099209547@qq.com>
2024-05-09 11:51:59 +0800

    转置 M*N矩阵

--- nacosTest/src/main/java/person/man/zhouyuqing/Link.java
+++ nacosTest/src/main/java/person/man/zhouyuqing/Link.java
@@ -117,6 +117,15 @@ public class Link {
     }
 
     public int[][] transpose(int[][] matrix) {
-
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return matrix;
+        }
+        int[][] ret = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                ret[j][i] = matrix[i][j];
+            }
+        }
+        return ret;
     }
 }
