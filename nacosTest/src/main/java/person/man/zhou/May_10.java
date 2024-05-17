db635ef544ee7c43b9b0ee3c008a4e22de40f972
sereinof <1099209547@qq.com>
2024-05-17 18:45:21 +0800

    整数转罗马数字

--- nacosTest/src/main/java/person/man/zhou/May_10.java
+++ nacosTest/src/main/java/person/man/zhou/May_10.java
@@ -43,6 +43,10 @@ public class May_10 {
 
     }
 
+    public List<String> letterCombinations(String digits) {
+
+    }
+
     public boolean isMatch(String s, String p) {
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         dp[0][0] = true;
@@ -51,8 +55,8 @@ public class May_10 {
         }
         for (int i = 1; i < s.length() + 1; i++) {
             for (int j = 1; j < p.length() + 1; j++) {
-                if (p.charAt(i - 1) == '*') {
-                    dp[i][j] =
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j - 2] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
                 } else {
                     dp[i][j] = dp[i - 1][j - 1] && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1));
                 }
