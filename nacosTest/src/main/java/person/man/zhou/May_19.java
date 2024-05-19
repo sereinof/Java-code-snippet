0e760cab64cad9428c3f51372766caec37a11a76
sereinof <1099209547@qq.com>
2024-05-19 16:07:23 +0800

    滑动窗口的最大值

--- nacosTest/src/main/java/person/man/zhou/May_19.java
+++ nacosTest/src/main/java/person/man/zhou/May_19.java
@@ -11,11 +11,53 @@ public class May_19 {
     int lenght = 0;
 
     public static void main(String[] args) {
-        List<String> res = new May_19().removeInvalidParentheses("()())()");
+        //  List<String> res = new May_19().removeInvalidParentheses("()())()");
+
+        int[] res = maxSlidingWindow(new int[]{1,3,1,2,0,5}, 3);
         System.out.println(res);
     }
-    public int[] maxSlidingWindow(int[] nums, int k) {
 
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if(k==1){
+            return nums;
+        }
+        Deque<Integer> deque = new ArrayDeque();
+        int[] res = new int[nums.length - k+1];
+        int j = 0;
+        for (int i = 0; i < k; i++) {
+            if (deque.isEmpty()) {
+                deque.add(i);
+            } else {
+                if (nums[deque.getLast()] < nums[i]) {
+                    while (!deque.isEmpty() && nums[deque.getLast()] < nums[i]) {
+                        deque.pollLast();
+                    }
+                    deque.add(i);
+                } else {
+                    deque.add(i);
+                }
+            }
+        }
+        res[j++] = nums[deque.getFirst()];
+        for (int i = k; i < nums.length; i++) {
+            int newl1 = i;
+            int needToremove = i - k;
+            int dequenL = deque.getFirst();
+            if (dequenL == needToremove) {
+                deque.pollFirst();
+            }
+            //队列前部分处理完毕
+            if (nums[newl1] > nums[deque.getLast()]) {
+                while (!deque.isEmpty() && nums[newl1] > nums[deque.getLast()]) {
+                    deque.pollLast();
+                }
+                deque.add(newl1);
+            } else {
+                deque.add(newl1);
+            }
+            res[j++] = nums[deque.getFirst()];
+        }
+        return res;
     }
 
 
@@ -42,7 +84,7 @@ public class May_19 {
             if (!res.isEmpty()) {
                 return res.stream().toList();
             }
-            for (String str:set) {
+            for (String str : set) {
                 for (int j = 0; j < str.length(); j++) {
                     if (str.charAt(j) == '(' || str.charAt(j) == ')') {
                         if (j > 0 && str.charAt(j) == s.charAt(j - 1))
