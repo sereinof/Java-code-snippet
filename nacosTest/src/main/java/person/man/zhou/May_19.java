278a5bc2503f3da9ca1a4d6e3a8a640765bb5aa0
sereinof <1099209547@qq.com>
2024-05-19 17:27:49 +0800

    模拟文本对齐

--- nacosTest/src/main/java/person/man/zhou/May_19.java
+++ nacosTest/src/main/java/person/man/zhou/May_19.java
@@ -13,16 +13,109 @@ public class May_19 {
     public static void main(String[] args) {
         //  List<String> res = new May_19().removeInvalidParentheses("()())()");
 
-        int[] res = maxSlidingWindow(new int[]{1,3,1,2,0,5}, 3);
+        int[] res = maxSlidingWindow(new int[]{1, 3, 1, 2, 0, 5}, 3);
         System.out.println(res);
     }
 
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        int l = 0;
+        int r = 0;
+        int least = 0;
+        int wordL = 0;
+        List<String> res = new ArrayList<>();
+        while (r < words.length) {
+            least--;
+            while (r < words.length && least < maxWidth) {
+                wordL += words[r].length();
+                least += words[r].length();
+                least++;
+                if (least > maxWidth) {
+                    wordL -= words[r].length();
+                    break;
+                }
+                r++;
+            }
+            r--;
+            res.add(produce(r - l + 1, maxWidth, l, r, wordL, words));
+            wordL = 0;
+            least = 0;
+            r++;
+            l = r;
+        }
+        return res;
+    }
+
+    public String produce(int n, int maxwidth, int l, int r, int wordL, String[] words) {
+        StringBuilder res = new StringBuilder();
+        int spaceN = maxwidth - wordL;
+        int[] arr = new int[n - 1];
+        if (r == words.length - 1) {
+            for (int i = 0; i <= r; i++) {
+                res.append(words[i]);
+                if (i != r) {
+                    res.append(" ");
+                }
+            }
+            for (int i = 0; i < maxwidth - res.length();i++) {
+                res.append(" ");
+            }
+            return res.toString();
+        }
+        if(n==1){
+            res.append(words[l]);
+            for(int i=0;i<maxwidth-words[l].length();i++){
+                res.append(" ");
+            }
+            return res.toString();
+        }
+        int everySpace = spaceN / (n - 1);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = everySpace;
+        }
+        int remain = spaceN % (n - 1);
+        for (int i = 0; i < remain; i++) {
+            arr[i]++;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            res.append(words[l + i]);
+            for (int j = 0; j < arr[i]; j++) {
+                res.append(" ");
+            }
+        }
+        res.append(words[r]);
+        return res.toString();
+    }
+
+    public int candy(int[] ratings) {
+        int[] L = new int[ratings.length];
+        int[] R = new int[ratings.length];
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                L[i] = L[i - 1] + 1;
+            } else {
+                L[i] = 1;
+            }
+        }
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                R[i] = R[i + 1] + 1;
+            } else {
+                R[i] = 1;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < ratings.length; i++) {
+            res += Math.max(R[i], L[i]);
+        }
+        return res;
+    }
+
     public static int[] maxSlidingWindow(int[] nums, int k) {
-        if(k==1){
+        if (k == 1) {
             return nums;
         }
         Deque<Integer> deque = new ArrayDeque();
-        int[] res = new int[nums.length - k+1];
+        int[] res = new int[nums.length - k + 1];
         int j = 0;
         for (int i = 0; i < k; i++) {
             if (deque.isEmpty()) {
