affbbc2ebe4bf6d2ede783405b443e894e1ffd35
sereinof <1099209547@qq.com>
2024-06-10 17:17:27 +0800

    罗马数字转整数

--- nacosTest/src/main/java/person/man/zhou/MyStack.java
+++ nacosTest/src/main/java/person/man/zhou/MyStack.java
@@ -20,12 +20,7 @@ class MyStack {
         map.put("C", 100);
         map.put("D", 500);
         map.put("M", 1000);
-        map.put("IV", 4);
-        map.put("IX", 9);
-        map.put("XL", 40);
-        map.put("XC", 90);
-        map.put("CD", 400);
-        map.put("CM", 900);
+
         int i = 0;
         int res = 0;
         while (i < s.length()) {
