c95e7bc89288f527718e2bd356d9771f34619145
sereinof <1099209547@qq.com>
2026-04-28 21:01:45 +0800

    接雨水

---
+++ nacosTest/src/main/java/person/man/findjobs/Trap.java
@@ -0,0 +1,12 @@
+package person.man.findjobs;
+
+public class Trap {
+    public int trap(int[] height) {
+// 接雨水 思路 将整体问题转化每一个点所能接住的雨水 。问题就变得简单了
+        int total = 0;
+        for (int i = 0; i <height.length ; i++) {
+
+
+        }
+    }
+}
