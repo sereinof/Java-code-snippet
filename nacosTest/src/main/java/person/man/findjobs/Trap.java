730510fbc8d2ae3d28bc2c12b28e341419e9bfee
sereinof <1099209547@qq.com>
2026-05-06 11:02:16 +0800

    接雨水通过版本

--- nacosTest/src/main/java/person/man/findjobs/Trap.java
+++ nacosTest/src/main/java/person/man/findjobs/Trap.java
@@ -13,28 +13,28 @@ public class Trap {
         //思路错了
         //对与每一根柱子 都需要找他左右两边的最大值，然后取最小值才是它实际能到达的高度
 // 接雨水 思路 将整体问题转化每一个点所能接住的雨水 。问题就变得简单了
-        //
+        //超时优化
         int total = 0;
-        for (int i = 1; i < height.length - 1; i++) {
-            int left = height[i];
-            int right = height[i];
-            int Lpiv = i - 1;
-            int Rpiv = i + 1;
-            while (Lpiv >= 0) {
-                if (height[Lpiv] > left) {
-                    left = height[Lpiv];
-                }
-                Lpiv--;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        int leftMax = height[0];
+        left[0] = 0;
+        for (int i = 1; i < height.length; i++) {
+            left[i] = Math.max(leftMax, height[i - 1]);
+            leftMax = left[i];
+        }
+
+        int rightMax = height[height.length - 1];
+        right[right.length - 1] = 0;
+        for (int i = height.length - 2; i >= 0; i--) {
+            right[i] = Math.max(height[i + 1], rightMax);
+            rightMax = right[i];
+        }
+        for (int i = 1; i < height.length - 1; i++) {//不遍历边缘的点
+            int low = Math.min(left[i], right[i]);
+            if (low > height[i]) {
+                total += (low-height[i]);
             }
-            ;
-            while (Rpiv <= height.length - 1) {
-                if (height[Rpiv] > right) {
-                    right = height[Rpiv];
-                }
-                Rpiv++;
-            }
-            int c = Math.min(left, right) - height[i];
-            total += c;
         }
         return total;
     }
