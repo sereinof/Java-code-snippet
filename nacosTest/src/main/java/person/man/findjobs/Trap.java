af76c064888e70575ba70072f78c631180503b73
sereinof <1099209547@qq.com>
2026-05-06 10:47:03 +0800

    接雨水超时版本

--- nacosTest/src/main/java/person/man/findjobs/Trap.java
+++ nacosTest/src/main/java/person/man/findjobs/Trap.java
@@ -1,49 +1,40 @@
 package person.man.findjobs;
 
 public class Trap {
+    private static int total;
+
     public static void main(String[] args) {//算法有缺陷 待进一步优化
-        int[] heights = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int[] heights = new int[]{4, 2, 0, 3, 2, 5};
         int res = trap(heights);
         System.out.printf(res + "");
     }
 
     public static int trap(int[] height) {
+        //思路错了
+        //对与每一根柱子 都需要找他左右两边的最大值，然后取最小值才是它实际能到达的高度
 // 接雨水 思路 将整体问题转化每一个点所能接住的雨水 。问题就变得简单了
+        //
         int total = 0;
-        for (int i = 0; i < height.length; i++) {
-            int left = 0;
-            int right = 0;
-            int a = i;//向左
-            int b = i;//香油
-            while (a > 0) {
-                int cand = a-1;
-                if (height[cand] >= height[cand+1]) {
-                    left = height[cand];
-                    a--;
-                } else {
-                    break;
+        for (int i = 1; i < height.length - 1; i++) {
+            int left = height[i];
+            int right = height[i];
+            int Lpiv = i - 1;
+            int Rpiv = i + 1;
+            while (Lpiv >= 0) {
+                if (height[Lpiv] > left) {
+                    left = height[Lpiv];
                 }
-
+                Lpiv--;
             }
-            while (b < height.length - 1) {
-                int cand = b+1;
-                if (height[cand] >= height[cand-1]) {
-                    right = height[cand];
-                    b++;
-                } else {
-                    break;
-int ad =9;
-int b= 90;
-
+            ;
+            while (Rpiv <= height.length - 1) {
+                if (height[Rpiv] > right) {
+                    right = height[Rpiv];
                 }
-
+                Rpiv++;
             }
-            int waterReally = Math.min(left, right);
-            if(waterReally<=height[i]){
-                continue;
-            }
-            total += (waterReally - height[i]);
-
+            int c = Math.min(left, right) - height[i];
+            total += c;
         }
         return total;
     }
