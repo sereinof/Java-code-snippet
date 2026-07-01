8fc29473669e5145b08117abb6bb4ff32012d6c3
sereinof <1099209547@qq.com>
2026-07-01 22:13:09 +0800

    H指数

---
+++ nacosTest/src/main/java/person/man/findjobs/Hindex.java
@@ -0,0 +1,31 @@
+package person.man.findjobs;
+
+public class Hindex {
+    public static void main(String[] args) {
+        int[] citaioms = new int[]{3, 0, 6, 1, 5};
+        int res = new Hindex().hIndex(citaioms);
+        System.out.println(res);
+    }
+
+    public int hIndex(int[] citations) {
+        if (citations.length == 0) {
+            return 0;
+        }
+//理解一下 h 个大于或等于h的 数 求这个h的最大值
+        //h最大也就 数组的长度了 先暴力一下吧
+        int maxHindex = 0;
+        for (int i = 1; i < citations.length + 1; i++) {
+            int count = 0;
+            for (int j = 0; j < citations.length; j++) {
+                if (citations[j] >= i) {
+                    count++;
+                    if (count >= i) {
+                        maxHindex = i;
+                        break;
+                    }
+                }
+            }
+        }
+        return maxHindex;
+    }
+}
