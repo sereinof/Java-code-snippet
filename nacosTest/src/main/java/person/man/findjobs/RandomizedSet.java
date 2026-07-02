ffdbe91b5b8ca7d75acdccea7111e56d9d5e074c
sereinof <1099209547@qq.com>
2026-07-02 11:29:54 +0800

    数据结构

--- nacosTest/src/main/java/person/man/findjobs/RandomizedSet.java
+++ nacosTest/src/main/java/person/man/findjobs/RandomizedSet.java
@@ -1,20 +1,40 @@
 package person.man.findjobs;
 
+import java.util.HashMap;
+import java.util.Random;
+
 public class RandomizedSet {
+    public static Random random = new Random();
+    private int[] valList = new int[2000001];
+    private HashMap<Integer, Integer> map = new HashMap<>();
+    private int index = 0;
 
     public RandomizedSet() {
 
     }
 
     public boolean insert(int val) {
-
+        if (map.containsKey(val)) {
+            return false;
+        } else {
+            map.put(val, index);
+            index++;
+            return true;
+        }
     }
 
     public boolean remove(int val) {
-
+        if (map.containsKey(val)) {
+            int indexToVal = map.get(val);
+            map.remove(val);
+            valList[indexToVal] = valList[--index];
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int getRandom() {
-
+   return  random.nextInt(index);
     }
 }
