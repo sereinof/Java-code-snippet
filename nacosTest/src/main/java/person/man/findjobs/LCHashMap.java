ccf5e0dbf26f8e79345482096263814596a40aa3
sereinof <1099209547@qq.com>
2026-08-03 21:38:26 +0800

    rangePractices

--- nacosTest/src/main/java/person/man/findjobs/LCHashMap.java
+++ nacosTest/src/main/java/person/man/findjobs/LCHashMap.java
@@ -12,7 +12,9 @@ public class LCHashMap {
     public static void main(String[] args) {
         String pattern = "abba";
         String s = "dog cat cat dog";
-        new LCHashMap().wordPattern(pattern, s);
+        // new LCHashMap().wordPattern(pattern, s);
+        int[] nums = new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        int res = new LCHashMap().longestConsecutive(nums);
     }
 
     public boolean wordPattern(String pattern, String s) {
@@ -71,7 +73,7 @@ public class LCHashMap {
             String s = strs[i];
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
-            String key =new String(chars);
+            String key = new String(chars);
             if (map.containsKey(key)) {
                 List<String> value = map.get(key);
                 value.add(strs[i]);
@@ -86,6 +88,92 @@ public class LCHashMap {
     }
 
     public boolean isHappy(int n) {
+        int num = n;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(num, 1);
+        while (true) {
+            int nextNum = 0;
+            while (num != 0) {
+                nextNum += (num % 10) * (num % 10);
+                num /= 10;
+            }
+            ;
+            if (map.containsKey(nextNum)) {
+                return false;
+            }
+            if (nextNum == 1) {
+                return true;
+            }
+            map.put(nextNum, 1);
+            num = nextNum;
+        }
+    }
 
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num)) {
+                if (i - map.get(num) <= k) {
+                    return true;
+                }
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+
+    public int longestConsecutive(int[] nums) {
+        int res = 0;
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], nums[i]);
+            }
+
+        }
+        for (int key : map.keySet()) {
+            int begin = key;
+            if (!map.containsKey(key - 1)) {
+                int len = 0;
+                while (map.containsKey(begin++)) {
+                    len++;
+                }
+                res = Math.max(res, len);
+            }
+        }
+
+        return res;
+    }
+
+    public List<String> summaryRanges(int[] nums) {
+        if(nums.length==0){
+            return  null;
+        }
+        List<String> res = new ArrayList<>();
+        int l = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] + 1 != nums[i]) {
+                StringBuilder range = new StringBuilder();
+                if (i - 1 == l) {
+                    range.append(nums[l]);
+                } else {
+                    range.append(nums[l]);
+                    range.append("->");
+                    range.append(nums[i - 1]);
+                }
+                res.add(range.toString());
+                l = i;
+            }
+        }
+        StringBuilder finalRange = new StringBuilder()
+        if (l == nums.length - 1) {
+            finalRange.append(nums[l]);
+        } else {
+            finalRange.append(nums[l]);
+            finalRange.append("->");
+            finalRange.append(nums[nums.length - 1]);
+        }
+        return res;
     }
 }
